15350363b4557f5518ed02a551e40a29ad1e7daf
Anhaki <m.anugrahhakiki@gmail.com>
2023-04-17 08:47:31 +0700

    Menambahkan Layout

---
+++ app/src/main/java/com/example/kulinerkita/Activity/TambahActivity.java
@@ -0,0 +1,16 @@
+package com.example.kulinerkita.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kulinerkita.R;
+
+public class TambahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+    }
+}
