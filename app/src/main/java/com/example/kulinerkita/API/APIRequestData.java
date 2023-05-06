a4a74144f6690cb6e991fc60ea12e9031b5a254c
Anhaki <m.anugrahhakiki@gmail.com>
2023-05-06 19:50:00 +0700

    Menambahkan apireqdata

---
+++ app/src/main/java/com/example/kulinerkita/API/APIRequestData.java
@@ -0,0 +1,39 @@
+package com.example.kulinerkita.API;
+
+import com.example.kulinerkita.Model.ModelResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface APIRequestData {
+    @GET("retrieve.php")
+    Call<ModelResponse> ardRetrieve();
+
+    @FormUrlEncoded
+    @POST("create.php")
+    Call<ModelResponse> ardCreate(
+            @Field("nama") String nama,
+            @Field("asal") String asal,
+            @Field("deskripsi_singkat") String deskripsi_singkat
+            );
+
+    @FormUrlEncoded
+    @POST("update.php")
+    Call<ModelResponse> ardUpdate(
+            @Field("id") String id,
+            @Field("nama") String nama,
+            @Field("asal") String asal,
+            @Field("deskripsi_singkat") String deskripsi_singkat
+    );
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    Call<ModelResponse> ardDelete(
+            @Field("id") String id
+    );
+
+
+}
