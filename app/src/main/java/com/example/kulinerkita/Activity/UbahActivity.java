cda7fa323b44ff8a3e80bd00e6a7af5af3ad5310
anhaki <m.anugrahhakiki@gmail.com>
2023-05-24 09:35:04 +0700

    Menambahkan Ubah Activity

--- app/src/main/java/com/example/kulinerkita/Activity/UbahActivity.java
+++ app/src/main/java/com/example/kulinerkita/Activity/UbahActivity.java
@@ -2,15 +2,94 @@ package com.example.kulinerkita.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.kulinerkita.API.APIRequestData;
+import com.example.kulinerkita.API.RetroServer;
+import com.example.kulinerkita.Model.ModelResponse;
 import com.example.kulinerkita.R;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class UbahActivity extends AppCompatActivity {
+    private String yId, yNama, yAsal, yDeskripsi;
+    private EditText etNama, etAsal, etDeskripsiSingkat;
+    private Button btnUbah;
+    private String nama, asal, deskripsiSingkat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubah);
+
+        Intent ambil = getIntent();
+        yId = ambil.getStringExtra("xId");
+        yNama = ambil.getStringExtra("xNama");
+        yAsal = ambil.getStringExtra("xAsal");
+        yDeskripsi = ambil.getStringExtra("xDeskripsi");
+
+        etNama = findViewById(R.id.et_name);
+        etAsal = findViewById(R.id.et_asal);
+        etDeskripsiSingkat = findViewById(R.id.et_deskripsi);
+        btnUbah = findViewById(R.id.btn_ubah);
+
+        etNama.setText(yNama);
+        etAsal.setText(yAsal);
+        etDeskripsiSingkat.setText(yDeskripsi);
+
+        btnUbah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nama =  etNama.getText().toString();
+                asal = etAsal.getText().toString();
+                deskripsiSingkat = etDeskripsiSingkat.getText().toString();
+
+                if(nama.trim().isEmpty()||asal.trim().isEmpty()||deskripsiSingkat.trim().isEmpty()){
+                    if(nama.trim().isEmpty()){
+                        etNama.setError("Nama Tidak Boleh Kosong");
+                    }
+                    if(asal.trim().isEmpty()){
+                        etAsal.setError("Asal Tidak Boleh Kosong");
+                    }
+                    if(deskripsiSingkat.trim().isEmpty()){
+                        etDeskripsiSingkat.setError("Deskripsi Tidak Boleh Kosong");
+                    }
+                }
+                else{
+                    UbahKuliner();
+                }
+            }
+        });
     }
+
+    private void UbahKuliner(){
+        APIRequestData ARD = RetroServer.koneksiRetrofit().create(APIRequestData.class);
+        Call<ModelResponse> proses = ARD.ardUpdate(yId, nama, asal, deskripsiSingkat);
+
+        proses.enqueue(new Callback<ModelResponse>() {
+            @Override
+            public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                String kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+
+                Toast.makeText(UbahActivity.this, "Kode : " + kode + "Pesan : " + pesan, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse> call, Throwable t) {
+                Toast.makeText(UbahActivity.this, "Gagal Menghubungi Server", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/kulinerkita/Adapter/AdapterKuliner.java
+++ app/src/main/java/com/example/kulinerkita/Adapter/AdapterKuliner.java
@@ -1,18 +1,31 @@
 package com.example.kulinerkita.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.kulinerkita.API.APIRequestData;
+import com.example.kulinerkita.API.RetroServer;
+import com.example.kulinerkita.Activity.MainActivity;
+import com.example.kulinerkita.Activity.UbahActivity;
 import com.example.kulinerkita.Model.ModelKuliner;
+import com.example.kulinerkita.Model.ModelResponse;
 import com.example.kulinerkita.R;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuliner> {
     private Context ctx;
@@ -54,6 +67,58 @@ public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuline
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvAsal = itemView.findViewById(R.id.tv_asal);
             tvDeskripsiSingkat = itemView.findViewById(R.id.tv_deskripsi);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                    pesan.setTitle("Perhatian");
+                    pesan.setMessage("Operaso apa yang akan dilakukan?");
+                    pesan.setCancelable(true);
+
+                    pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            hapusKuliner(tvId.getText().toString());
+                            dialog.dismiss();
+                        }
+                    });
+                    pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent pindah = new Intent(ctx, UbahActivity.class);
+                            pindah.putExtra("xId", tvId.getText().toString());
+                            pindah.putExtra("xNama", tvNama.getText().toString());
+                            pindah.putExtra("xAsal", tvAsal.getText().toString());
+                            pindah.putExtra("xDeskripsi", tvDeskripsiSingkat.getText().toString());
+                            ctx.startActivity(pindah);
+                        }
+                    });
+                    pesan.show();
+                    return false;
+                }
+            });
+        }
+
+        private void hapusKuliner(String idKuliner){
+            APIRequestData ARD = RetroServer.koneksiRetrofit().create(APIRequestData.class);
+            Call<ModelResponse> proses = ARD.ardDelete(idKuliner);
+
+            proses.enqueue(new Callback<ModelResponse>() {
+                @Override
+                public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                    String kode = response.body().getKode();
+                    String pesan = response.body().getPesan();
+
+                    Toast.makeText(ctx, "Kode : " + kode + ", Pesan : " + pesan, Toast.LENGTH_SHORT).show();
+                    ((MainActivity) ctx).retrieveKuliner();
+                }
+
+                @Override
+                public void onFailure(Call<ModelResponse> call, Throwable t) {
+                    Toast.makeText(ctx, "Gagal menghubungi server", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
